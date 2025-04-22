0bfc38996e4cb61509b92f8d61028436191911d0
deokchan <63943073+deokchan@users.noreply.github.com>
2025-04-22 15:17:39 +0900

    Add files via upload

---
+++ YangCho.js
@@ -0,0 +1,110 @@
+﻿odi = [
+"https://blog.kakaocdn.net/dn/d4AQdP/btsNufvhLgc/ZWG4kksZk2qv3m3Krba9R1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W16_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W16_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/b8gdSF/btsNsEotu2S/k9duhG7PJleJxua1ByN0SK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M11_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M11_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/beAlWM/btsNtrcrS8n/z9dAvU04OzbqkgQTpp8nT1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W3_copilot_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W3_copilot_Suno",
+"https://blog.kakaocdn.net/dn/lBukQ/btsNt5Gvp6Q/BjhQNeLajTtajoYuSWIveK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M14_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M14_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/RqLtk/btsNtLV4Pua/dtcmkZg8A3OHqp5nmS8aVK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W11_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W11_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/bd7N9n/btsNum2gKfv/yoFS5Qx7w36JpM1qkvP2yK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_WM1_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_WM1_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/uW8cO/btsNuiFzTXo/1foNp3VB3sATKGJv6R8wCk/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M16_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M16_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/cADvmI/btsNuwcubaa/wmkI1Ke8XxSsWZTiwmeCV0/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W11_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W11_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/tkzDi/btsNt1j35gg/qoef5tyn9I1sbN8pNKm7A1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M13_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M13_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/XNgJ6/btsNt2XFXfg/pFeUUICDxFdajl9e8Kx3ZK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W15_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W15_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/dihuLM/btsNuwQ6T6E/wEstQGtBvWgxPDeHXwc5f0/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M11_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M11_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/XPlrA/btsNuIDo5cF/kYFF18HxpDZxM6YkRsADCK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W14_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W14_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/MyFte/btsNuYGk9ZO/gRSJUzIkL9KpnjSgsqKpqk/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M15_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M15_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/cE3yf5/btsNt5fr3CD/SGlUnZPkV0N5zbNXswvQM1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W17_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W17_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/bxPZKG/btsNtt2tF9M/jCBUpStwCrSfhwT0phN9KK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M14_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M14_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/IwU3M/btsNu29B2hP/2yFnpUvWX2cuyeKJ8WCEW1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W16_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W16_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/WNzkb/btsNuDPYFhZ/zNQ28ra1pVG6joT5N8CEE1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_M12_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_M12_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/d012Dt/btsNrsvlzqW/0qfFksDioqeThTEed6maI1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W13_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W13_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/bip16h/btsNucyC5Bt/L71l8IQlOPEniKkvErv5Tk/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W17_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W17_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/b0XSxq/btsNt3aSD2M/8gLyt3z0plqyPHpXlW4S91/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M12_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M12_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/bhKjoC/btsNtNM97PA/mDJQQ5oisOQaFufd928DsK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W13_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W13_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/brQ2t4/btsNt6k6D1O/FeMP4M11Q4bKoLa58N9jkK/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M13_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M13_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/sjwAT/btsNu0RHGf7/6Ft1gFlLs9hyPsg62T46y0/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W14_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W14_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/XMdNa/btsNuGeRBBm/PKYuJoWRkHUHXy1jERum20/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M2_GPT_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M2_GPT_Suno",
+"https://blog.kakaocdn.net/dn/cCjZqx/btsNuvEEsTH/2IBRckvgXNMGyk54OsqRV1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W15_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W15_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/cb4QPW/btsNugnvcFW/yihxxcEz0c435LnH922Ja1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_M1_GPT_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_M1_GPT_Suno",
+"https://blog.kakaocdn.net/dn/bQTZZX/btsNuoMzddy/7XECuf02Ha9TUfaRM12b71/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W12_copilot_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W12_copilot_riffusion",
+"https://blog.kakaocdn.net/dn/dBl5YO/btsNt5T7jja/XtBcLTPDUoFZpLw9JP5Ymk/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W12_GPT_riffusion.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W12_GPT_riffusion",
+"https://blog.kakaocdn.net/dn/dhladZ/btsNtNl56dV/4MYcU365IF2LG2sZuEQe30/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W2_copilot_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W2_copilot_Suno",
+"https://blog.kakaocdn.net/dn/bY9Uop/btsNt1xCZox/ou3If0UZkQlvwPE9dW6Lr1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W1_GPT_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W1_GPT_Suno",
+"https://blog.kakaocdn.net/dn/vKQKl/btsNt3aSBYN/DC3e3R8aKvu1diUOOLMW5k/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EB%A7%8C%EB%82%A8_W1_copilot_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 만남_W1_copilot_Suno",
+"https://blog.kakaocdn.net/dn/oL7mI/btsNuvxTqc2/3IuOrWEDJ3LPSGBBCKvRM1/%EC%96%91%EC%B4%88%2049%ED%9A%8C%EC%9D%98%20%EC%9C%A0%EC%B1%84%EA%BD%83%20%EC%9D%B4%EC%95%BC%EA%B8%B0_W2_GPT_Suno.mp3?attach=1&knm=tfile.mp3|양초 49회의 유채꽃 이야기_W2_GPT_Suno",
+]
+
+song = [
+"<table style='BACKGROUND:lightpink repeat fixed left top' cellpadding='10' width=100%><tr valign=middle><td class='lt'>\
+<P class='mid'><br>\
+<b>양초 49회의 유채꽃 만남</b><br><br>\
+\
+[Verse 1]  <br>\
+봄빛 물결 춤추는 유채꽃 사잇길,  <br>\
+노랑 향기 가득한 이 들판 위에.  <br>\
+바람 속에 너울대는 꽃물결 따라,  <br>\
+파랑 하늘 아래 꿈이 피어나네요.<br><br>\
+\
+[Chorus]  <br>\
+빨강, 노랑, 파랑의 세상,  <br>\
+우리의 동심이 춤추는 색깔들.  <br>\
+흩날리는 봄빛 속의 설렘이,  <br>\
+그날을 기다리며 가슴에 피어나네요.<br><br>\
+\
+[Verse 2]  <br>\
+노랑 의자 위에 함박꽃 웃음,  <br>\
+빨강 의자에 쌓이는 옛 우정들.  <br>\
+소나무 아래에 바다가 속삭이며,  <br>\
+고향의 기억 속 추억을 불러오네요.<br><br>\
+\
+[Chorus]  <br>\
+빨강, 노랑, 파랑의 세상,  <br>\
+초등 시절의 이야기가 피어나는 곳.  <br>\
+곧 다가올 친구들과의 만남에,<br>\
+설렘이 가슴 속 깊이 새겨지네요.<br><br>\
+\
+[Outro]  <br>\
+소나무 아래에 빨강 노랑의 벤치,  <br>\
+파랑 바다와 함께 펼쳐지는 동심.  <br>\
+우리가 다시 만나 웃음꽃 피울 때,  <br>\
+봄날의 추억은 더욱 빛날 거예요.<br>\
+</td></tr></table>",
+"<table style='BACKGROUND:lightpink repeat fixed left top' cellpadding='10' width=100%><tr valign=middle><td class='lt'>\
+<P class='mid'><br>\
+<b>양초 49회의 유채꽃 이야기</b><br><br>\
+[Intro]<br>\
+노랑 빛깔 들판 위에 봄바람이 웃고<br>\
+유채꽃 사이로 전해진 이름 하나  <br>\
+보고 싶다는 단톡방의 인사에  <br>\
+햇살보다 먼저 피어나는 설레임  <br><br>\
+[Verse 1]<br>\
+우리들의 눈길을 시샘하는  <br>\
+봄바람 질투의 살랑임에<br>\
+장난어린 유채화(花)는 넘실대며 <br>\
+파랑 하늘에 수채화를 그린다<br><br>\
+[Chorus]<br>\
+유채꽃보다 진한 노랑 의자에  <br>\
+오래된 우정의 얼굴들이 모여  <br>\
+동심의 함박 웃음꽃을 피우며<br>\
+사진 한 장의 추억을 만든다  <br><br>\
+[Verse 2] <br>\
+빨강 의자에 모여든 이야기에  <br>\
+그때 그 시절 장난이 피어오르고<br>\
+어릴 적의 빨 노 파를 배경으로<br>\
+양초 49회, 추억을 새긴다  <br><br>\
+[Bridge]<br>\
+2년의 시간도, 계절의 바람도  <br>\
+우리를 갈라놓지는 못했네  <br>\
+그저 한 자리에 모이면  <br>\
+우리는 여전히 초등의 봄날  <br><br>\
+[Chorus]<br>\
+유채꽃보다 진한 노랑 의자에  <br>\
+양초 49회, 웃음꽃이 활짝  <br>\
+빨강 노랑 파랑의 마음 속에서  <br>\
+우리는 그때로 돌아가는 중  <br><br>\
+[Outro]<br>\
+소나무 아래, 벤치에 앉아  <br>\
+고향의 파랑 바다를 바라보면  <br>\
+지금도 펼쳐지는 동심의 이야기  <br>\
+유채꽃은 양초 49회의 그날처럼..<br>\
+</td></tr></table>"
+] 
