695565857e0ce607795639a3ac655b1348a79940
deokchan <63943073+deokchan@users.noreply.github.com>
2025-06-08 14:11:22 +0900

    Add files via upload

---
+++ Monogol.js
@@ -0,0 +1,143 @@
+﻿const title0 = "모노골 단합가"
+
+odi = [
+"https://blog.kakaocdn.net/dn/n162j/btsOskWZmLB/pkiQvZjYnNWgSKGaQOVxkK/0%20%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M1_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M1_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/r7KRF/btsOrVclK5p/r1VBdU3blsewMhBTD0qDo0/0%20%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_MW1_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가 (떼창) _MW1_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/bapVRb/btsOtIh2Iq7/Puqsxzk8SGXsqB1ltmuNwk/1%20%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M3_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M3_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/WSfJ2/btsOr3BbFLA/kPRiuzIcJn2P4elKbFUHt1/%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M7_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가 (떼창) _M7_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/blr8lc/btsOrvdZlma/xGGmg2gVyQXHWVxkjxQ8n0/1%20%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M8_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가 (떼창) _M8_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/c5Bcfj/btsOryaGrp0/8GMAnqS0tN8ca1bbpPWNx0/%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M2_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M2_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/cSzYyv/btsOsDWf2JL/D2OkDcuHIqscWydSHGzYtK/0%20%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M5_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M5_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/bzQCua/btsOsT5C6hk/0PHn821bzcy6kFr50ueAOK/%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_MW2_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가 (떼창) _MW2_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/biQKj0/btsOtJaaW6c/G1zwGNEisbMKK2TTm5df7K/%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M4_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M4_ChatGPT_Suno4.5",
+"https://blog.kakaocdn.net/dn/crRwLG/btsOttFp2qM/f6boSM4WKdScCeFcAikKc1/%EB%AA%A8%EB%85%B8%EA%B3%A8%20%EB%8B%A8%ED%95%A9%EA%B0%80_M6_ChatGPT_Suno4.5.mp3?attach=1&knm=tfile.mp3|모노골 단합가_M6_ChatGPT_Suno4.5",
+]
+
+song = [
+"<table class='box' style=\"BACKGROUND:#ccffff url('https://blog.kakaocdn.net/dn/J6AUP/btsN1mg4f8R/qdQwD49iqo2yK9G590hPCK/img.png') repeat fixed center top\" cellpadding='8' width=100%><tr valign=middle><td class='lt'>\
+<P class='mid'>\
+<font color='blue'><b>모노골 단합가</b></font><br><br>\
+\
+천년의 향기, <br>\
+솔향이 은은히 퍼지는 날에<br><br>\
+\
+소나무의 피톤치드, <br>\
+마음껏 들이마시며<br><br>\
+\
+벚꽃 향기 흩날리는 봄의 향연 따라<br>\
+오늘도 우리는 <br>\
+모노골 오솔길을 정답게 걷는다!<br><br><br>\
+\
+\
+숨 가쁘게 오르는 오르막길,<br>\
+다리 휘청대는 고갯길에도 <br>\
+웃음꽃 피네<br><br>\
+\
+고갯마루 넘어서면 반기는 그곳,<br>\
+꽃과 새들이 들려주는 모노골의 노래!<br><br>\
+\
+기운 내어 앞으로, 함께 가자!<br>\
+피톤치드 내음 따라 힘차게 걷자!<br><br>\
+\
+한 걸음 한 걸음 쌓이는 정<br>\
+우린 오늘도 <br>\
+같이 걷는 변치 않을 친구로다!<br><br><br>\
+\
+\
+샘터의 물 한 모금에 땀을 식히며<br>\
+모노골 벤치에선 정담의 꽃 피어난다<br><br>\
+\
+친구와 같이 걷는 이 길이 좋구나<br>\
+모노골 길에는 우리의 우정이 흐른다!<br><br>\
+\
+기운 내어 앞으로, 함께 가자!<br>\
+햇살 속에 웃으며 힘차게 걷자!<br><br>\
+\
+바람을 벗 삼고 꽃을 노래하며<br>\
+우리의 단합은 모노골에서 자라난다<br><br><br>\
+\
+\
+비바람 불고 눈보라 친다 해도<br>\
+이 길을 함께 걷는다면 두렵지 않으리<br><br>\
+\
+산도 들도, 나무들도 기억하리라<br>\
+모노골의 친구들이 지나간 이 길을!<br><br>\
+\
+함께라서 더욱 정다운 우리의 이름들..<br>\
+덕찬아! 상경아! 상복아! 정한아!<br>\
+진섭아! 헌길아! 모두 모두 불러본다!<br><br>\
+\
+이 세상 소풍이 끝나는 그날까지 <br>\
+같이 갈 친구들..<br><br>\
+\
+오늘도 우리는 <br>\
+모노골 오솔길을 정답게 걷는다~!<br>\
+</td></tr></table>",
+"<table class='box' style=\"BACKGROUND:#ccffff url('https://blog.kakaocdn.net/dn/cUp0rL/btsN2ckyc1D/BPLfEWUUqnn1dvGE9u6hLK/img.png') repeat fixed center top\" cellpadding='8' width=100%><tr valign=middle><td class='lt'>\
+<P class='mid'>\
+<font color='blue'><b>모노골 단합가 (떼창) </b></font><br><br>\
+\
+천년의 향기, <br>\
+솔향이 은은히 퍼지는 날에<br><br>\
+\
+소나무의 피톤치드, <br>\
+마음껏 들이마시며<br><br>\
+\
+벚꽃 향기 흩날리는 봄의 향연 따라<br>\
+오늘도 우리는 <br>\
+모노골 오솔길을 정답게 걷는다!<br><br><br>\
+\
+\
+숨 가쁘게 오르는 오르막길,<br>\
+다리 휘청대는 고갯길에도 <br>\
+웃음꽃 피네<br><br>\
+\
+고갯마루 넘어서면 반기는 그곳,<br>\
+꽃과 새들이 들려주는 모노골의 노래!<br><br><br>\
+\
+\
+[Chorus – Group Chant (떼창)]<br>\
+기운 내어 앞으로, 함께 가자!<br>\
+피톤치드 내음 따라 힘차게 걷자!<br><br>\
+\
+한 걸음 한 걸음 쌓이는 정<br>\
+우린 오늘도 <br>\
+같이 걷는 변치 않을 친구로다!<br><br><br>\
+\
+\
+샘터의 물 한 모금에 땀을 식히며<br>\
+모노골 벤치에선 정담의 꽃 피어난다<br><br>\
+\
+친구와 같이 걷는 이 길이 좋구나<br>\
+모노골 길에는 우리의 우정이 흐른다!<br><br><br>\
+\
+\
+[Chorus – Group Chant (떼창)]<br>\
+기운 내어 앞으로, 함께 가자!<br>\
+햇살 속에 웃으며 힘차게 걷자!<br><br>\
+\
+바람을 벗 삼고 꽃을 노래하며<br>\
+우리의 단합은 모노골에서 자라난다<br><br><br>\
+\
+\
+비바람 불고 눈보라 친다 해도<br>\
+이 길을 함께 걷는다면 두렵지 않으리<br><br>\
+\
+산도 들도, 나무들도 기억하리라<br>\
+모노골의 친구들이 지나간 이 길을!<br><br>\
+\
+함께라서 더욱 정다운 우리의 이름들..<br>\
+덕찬아! 상경아! 상복아! 정한아!<br>\
+진섭아! 헌길아! 모두 모두 불러본다!<br><br>\
+\
+이 세상 소풍이 끝나는 그날까지 <br>\
+같이 갈 친구들..<br><br>\
+\
+오늘도 우리는 <br>\
+모노골 오솔길을 정답게 걷는다~!<br><br><br>\
+\
+\
+[Chorus – Group Chant (떼창)]<br>\
+모노골! 친구들이여! 영원하리라!<br>\
+</td></tr></table>",
+]
