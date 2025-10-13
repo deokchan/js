b9e524746e2ce5729d5484da2ea3a4d61bb2380e
deokchan <63943073+deokchan@users.noreply.github.com>
2025-10-13 23:42:32 +0900

    Add files via upload

---
+++ AI-IoT_Song_251013.js
@@ -0,0 +1,304 @@
+﻿const title0 = "AI-IoT 기반 어르신 건강송"
+
+odi = [
+"https://cdn1.suno.ai/ee37a5fb-5044-4a4b-a9f3-43fa8258086b.mp3|AI-loT 기반 어르신 건강관리사업 (포크)_MW07|307",
+"https://cdn1.suno.ai/9e57c0e3-2267-4dcd-84ac-2106042ead60.mp3|AI-loT 기반 어르신 건강관리사업 (트롯 행진)_MW01|290",
+"https://cdn1.suno.ai/05e6118f-0e61-4774-a12d-0bf76970899f.mp3|AI-loT 기반 어르신 건강관리사업 (일렉트로 댄스)_M03|231",
+"https://cdn1.suno.ai/21ca928c-c7ed-40b6-bfa1-a774fd806699.mp3|AI-loT 기반 어르신 건강관리사업 (발라드)_M05|276",
+"https://cdn1.suno.ai/334334ce-fd69-452c-91a8-174573827737.mp3|AI-loT 기반 어르신 건강관리사업 (포크)_MW06|225",
+"https://cdn1.suno.ai/b75e3971-72d3-4565-a6cd-3611744bde8a.mp3|AI-loT 기반 어르신 건강관리사업 (트로트)_MW21|299",
+"https://cdn1.suno.ai/4c970072-917d-4bf4-a8ce-4e9abae27bae.mp3|AI-loT 기반 어르신 건강관리사업 (행진)_MW16|242",
+"https://cdn1.suno.ai/2fcff751-0f91-47d2-9bcb-694a018b1ec5.mp3|AI-loT 기반 어르신 건강관리사업 (바이올린 신스팝)_MW11|",
+"https://cdn1.suno.ai/c7cc5683-2fa8-46e0-989c-40998eb65f51.mp3|AI-loT 기반 어르신 건강관리사업 (트로트)_MW20|332",
+"https://cdn1.suno.ai/a08526e5-a3b1-4506-8100-0335df8dbeac.mp3|AI-loT 기반 어르신 건강관리사업 (심포닉록)_MW19|",
+"https://cdn1.suno.ai/65636c18-b66b-442a-97bb-65603bd3f443.mp3|AI-loT 기반 어르신 건강관리사업 (클래식)_MW08 (Edit)|",
+"https://cdn1.suno.ai/fe77ddc9-1cec-4c04-a299-b8824e429732.mp3|AI-loT 기반 어르신 건강관리사업 (브라스)_WM02|314",
+"https://cdn1.suno.ai/6e647c31-0939-4ec9-add7-819b04ab536e.mp3|AI-loT 기반 어르신 건강관리사업 (에모팝)_MW14|307",
+"https://cdn1.suno.ai/1f002591-d8f5-4911-af86-6a432a962c7b.mp3|AI-loT 기반 어르신 건강관리사업 (발라드 75BPM)_MW10|",
+"https://cdn1.suno.ai/beb9ed65-be91-489d-894b-7a1b1acf579f.mp3|AI-IoT 어르신 건강송 (트롯 행진)_MW07|307",
+"https://www.dropbox.com/scl/fi/otnr4oaipmapzb80k1f6v/AI-IoT-_MW09.mp3?rlkey=9rxm2k5j240wnb5mk8h4m5hgm&st=q7cnfe6h&raw=1|AI-IoT 어르신 건강송 (브라스)_MW09|253",
+"https://cdn1.suno.ai/d29dbea0-b34e-44a5-909e-c85e318491f1.mp3|AI-IoT 어르신 건강송 (동요)_MW06|238",
+"https://cdn1.suno.ai/7663400a-7877-4275-802d-ab9e0ce58a4b.mp3|AI-IoT 어르신 건강송 (트로트)_MW13|305",
+"https://cdn1.suno.ai/457e8ba1-5cda-4c5f-be78-dcda7d3e58d6.mp3|AI-IoT 어르신 건강송 (트롯 행진)_MW01|221",
+"https://cdn1.suno.ai/a50549c3-5059-4beb-a849-d4c0091b421b.mp3|AI-IoT 어르신 건강송 (행진)_MW08|271",
+"https://cdn1.suno.ai/308e2f7c-0bc2-4866-8cdf-9f07a74d3110.mp3|AI-IoT 어르신 건강송 (뉴웨이브)_M02|250",
+"https://cdn1.suno.ai/63620ad5-ba66-4943-ab1b-fb73bff4c28c.mp3|AI-IoT 어르신 건강송 (트롯 행진)_MW05|252",
+"https://cdn1.suno.ai/99d4ed96-2e43-44e3-8f3c-3ecfd4239678.mp3|AI-IoT 어르신 건강송 (트로트)_MW12|291",
+"https://cdn1.suno.ai/dcb8eeb7-63f6-47b8-936f-14e8d6f4dfb8.mp3|AI-IoT 어르신 건강송 (트롯 행진)_M01|284",
+"https://cdn1.suno.ai/4cd6d3ae-a5cb-45f2-81cd-0329c6cf37c7.mp3|AI-IoT 어르신 건강송 (트롯 행진)_WM02|224",
+]
+
+song = [
+"<table class='box' style=\"BACKGROUND:#ccffff url('https://blog.kakaocdn.net/dn/Xj6h3/btsOxjDAXcz/cen6SOE7KJFtvqXrwhQkEk/img.png') repeat fixed center center\" cellpadding='8' width=100%><tr valign=middle><td class='lt'>\
+<P class='mid'>\
+<font color='blue'><b>AI-loT 기반 어르신 건강관리사업</b></font><br><br>\
+\
+'어르신'이 되었다면 건강 관리하세요.<br>\
+AI 시대답게 loT 기반으로 하세요.<br><br>\
+\
+<img src='https://i.imgur.com/yrtA3EI.jpeg' width=100% title='📷 01 어르신 02'><br><br>\
+\
+쉽고 편하게, <br>\
+정확하게 기록 관리하세요.<br>\
+사물 인터넷, <br>\
+블루투스가 알아서 해준다네요~<br><br><br>\
+\
+\
+1일 미션의 걷기 운동, <br>\
+포인트는 5점이래요.<br>\
+매일매일 30분 이상, <br>\
+5,000 보는 걸어 봐요.<br><br>\
+\
+<img src='https://i.imgur.com/HXJNyFv.jpeg' width=32% title='📷 01 걷기 01'> \
+\
+<img src='https://i.imgur.com/dIqO3ja.jpeg' width=32% title='📷 01 걷기 02'> \
+\
+<img src='https://i.imgur.com/Qr2twfb.jpeg' width=32% title='📷 01 걷기 03'><br><br>\
+\
+집안에서 문밖까지 <br>\
+가장 힘든 구간이지만..<br>\
+여기 고비만 극복하면 <br>\
+5,000 보는 금방이래요.<br><br><br>\
+\
+\
+1일 미션의 혈압 측정, <br>\
+포인트도 5점이래요.<br>\
+팔꿈치 1~2cm 위로 <br>\
+커프를 단단히 감아요.<br><br>\
+\
+<img src='https://i.imgur.com/ineQWqL.jpeg' width=32% title='📷 01 혈압 03'> \
+\
+<img src='https://i.imgur.com/ORk8uP9.jpeg' width=32% title='📷 01 혈압 05'> \
+\
+<img src='https://i.imgur.com/nMSxQGV.jpeg' width=32% title='📷 01 혈압 06'><br><br>\
+\
+수축기 130은 경계, <br>\
+140 이상은 위험하고요.<br>\
+이완기는 80이 경계, <br>\
+90이 넘으면 위험하대요.<br><br><br>\
+\
+\
+맥박 수는 <br>\
+60에서 100 사이가 정상이고요.<br>\
+60 아래는 '서맥', <br>\
+100이 넘어가면 '빈맥'입니다.<br><br>\
+\
+<img src='https://i.imgur.com/C7ZFvOa.png' width=45% title='📷 02 20250628_123400_craiyon'> \
+\
+<img src='https://i.imgur.com/AA3nTOp.jpeg' width=53% title='📷 01 혈압 02'><br><br>\
+\
+신생아는 120~140, <br>\
+운동선수는 50이래요.<br>\
+나이 들면 느려질 수 있다니 <br>\
+너무 걱정 말아요.<br><br><br>\
+\
+\
+1일 미션의 <br>\
+악력계와 건강 밴드는 2점이래요.<br>\
+악력계를 꽉~꽉 눌러서 <br>\
+손아귀 힘을 키워요~~<br><br>\
+\
+<img src='https://i.imgur.com/c64iljP.jpeg' width=64% title='📷 EZ그립 실리콘 악력기'> \
+\
+<img src='https://i.imgur.com/5ZK7w4c.jpeg' width=34% title='📷 건강밴드 운동 안내 도표'><br><br>\
+\
+건강 밴드 11개 동작은, <br>\
+10번씩 3세트래요.<br>\
+즐겁게 운동하니.. <br>\
+집 나갔던 근육이 돌아오네요~~<br><br><br>\
+\
+\
+주간 미션의 혈당 측정, <br>\
+포인트는 5점이래요.<br>\
+공복 혈당은, <br>\
+최소한 8시간 금식하세요.<br><br>\
+\
+<img src='https://i.imgur.com/nN7zAak.jpeg' width=32% title='📷 01 혈당 01'> \
+\
+<img src='https://i.imgur.com/Sb8sr1O.jpeg' width=32% title='📷 01 혈당 03'> \
+\
+<img src='https://i.imgur.com/QsKQ6O8.jpeg' width=32% title='📷 01 혈당 04'><br><br>\
+\
+탈수 방지용 물은 섭취, <br>\
+다른 건 안 된답니다.<br>\
+100 이상은 경계, <br>\
+126 넘으면 위험 단계래요.<br><br><br>\
+\
+\
+식후 혈당은 <br>\
+식사 시작 시점부터 두 시간 뒤,<br>\
+하지만 여덟 시간까지는 <br>\
+식후로 간주합니다.<br><br>\
+\
+<img src='https://i.imgur.com/0LCI7Jm.png' width=68% title='📷 02 20250630_223252_craiyon'> \
+\
+<img src='https://i.imgur.com/Urnk2dt.jpeg' width=30% title='📷 01 혈당 02'><br><br>\
+\
+140 이상은 경계, <br>\
+200 넘으면 위험 단계이고요~<br>\
+당화 혈색소는 <br>\
+7% 넘어가면 위험 신호랍니다.<br><br><br>\
+\
+\
+주간 미션의 체중 측정은 <br>\
+2점 밖에 안 되네요.<br>\
+체중 감량보다는 <br>\
+체지방률 감소에 신경 쓰세요~<br><br>\
+\
+<img src='https://i.imgur.com/hpnwwu4.jpeg' width=32% title='📷 01 체중 03'> \
+\
+<img src='https://i.imgur.com/cw3EbbH.jpeg' width=32% title='📷 01 체중 05'> \
+\
+<img src='https://i.imgur.com/JWvqbCn.jpeg' width=32% title='📷 01 체중 06'><br><br>\
+\
+남자는 18% 정상, <br>\
+25는 과체중, 이상은 비만이고요.<br>\
+여자는 25% 정상, <br>\
+30은 과체중, 넘으면 비만이래요.<br><br><br>\
+\
+\
+건강 챙기면서 포인트 따는 <br>\
+활기찬 노년이 됩니다.<br><br>\
+\
+<img src='https://i.imgur.com/hGIzb3G.jpeg' width=32% title='📷 01 포인드 01'> \
+\
+<img src='https://i.imgur.com/plJuGRG.jpeg' width=32% title='📷 01 포인드 02'> \
+\
+<img src='https://i.imgur.com/8Qq0dqI.jpeg' width=32% title='📷 01 포인드 03'><br><br>\
+\
+뽕도 따고 임도 보고, <br>\
+도랑 치고 가재 잡고..<br>\
+꿩~ 먹고 알 먹고, <br>\
+굿도 보고 떡도 먹어요~<br><br>\
+\
+<img src='https://i.imgur.com/km6xE80.jpeg' width=23% title='📷 01 미션 01'> \
+\
+<img src='https://i.imgur.com/MLr6x2f.jpeg' width=23% title='📷 01 미션 02'> \
+\
+<img src='https://i.imgur.com/zwruPYb.jpeg' width=23% title='📷 01 미션 03'> \
+\
+<img src='https://i.imgur.com/qHKHgCM.jpeg' width=23% title='📷 01 미션 04'><br><br>\
+\
+일거 양득의 건강 관리, <br>\
+쉽고 즐겁게 함께 해요.<br><br>\
+\
+'어르신 인생'을.. <br>\
+건강하고 행복하게 살아갑시다~~<br><br>\
+\
+<img src='https://i.imgur.com/6MPYp7j.jpeg' width=100% title='📷 01 어르신 04'><br>\
+</td></tr></table>",
+"<table class='box' style=\"BACKGROUND:#ccffff url('https://blog.kakaocdn.net/dn/w2O7G/btsOwvLzvhs/ZIe7tvEvh6OwCShCEqEJVK/img.jpg') repeat fixed center top\" cellpadding='8' width=100%><tr valign=middle><td class='lt'>\
+\
+<P class='mid'>\
+<font color='blue'><b>AI-IoT 어르신 건강송</b></font><br><br></P><P class='mid2'>\
+'어르신'이 되셨다면 건강 챙기세요<br>\
+AI 시대, 이제는 IoT로 해봐요<br><br>\
+\
+<img src='https://i.imgur.com/HhMxJK8.jpeg' width=100% title='📷 01 어르신 03'><br><br>\
+\
+쉽고 편하게, 정확하게 기록 관리!<br>\
+사물 인터넷, 블루투스가 <br>\
+알아서 해준답니다<br><br><br>\
+\
+\
+1일 미션, <br>\
+걷기 운동 포인트는 5점이래요<br>\
+매일매일 30 분, 5,000 보를 걸어 봐요<br><br>\
+\
+집안에서 문밖까지, 가장 힘든 첫걸음,<br>\
+고비만 넘기면 5,000 보는 금방이래요<br><br><br>\
+\
+\
+1일 미션, <br>\
+혈압 측정 포인트도 5점이래요<br>\
+팔꿈치 위로 커프를 꽉~, <br>\
+바르게 감아줘요<br><br>\
+\
+수축기 130은 경계, 140 넘으면 위험,<br>\
+이완기 80은 경계, 90 넘으면 위험해요<br><br><br>\
+\
+\
+맥박 수는 60에서 100 사이가 정상<br>\
+60 아래는 '서맥', <br>\
+100 넘어가면 '빈맥'이래요<br><br>\
+\
+신생아는 빠르고, <br>\
+운동선수는 느리다네요<br>\
+나이 들면 조금 느려도 너무 걱정 말아요<br><br><br>\
+\
+\
+1일 미션, <br>\
+악력계와 건강 밴드는 2점이래요<br>\
+악력계를 꼭 쥐고, 손아귀 힘 키워봐요<br><br>\
+\
+건강 밴드 11개 동작, 10번씩 3세트<br>\
+즐겁게 운동하면, <br>\
+집 나간 근육이 돌아와요<br><br><br>\
+\
+\
+주간 미션, <br>\
+혈당 측정 포인트는 5점이래요<br><br>\
+\
+공복 혈당은 최소 8시간 금식이 필요해요<br>\
+물은 마셔도 돼요, <br>\
+다른 건 먹으면 안 돼요.<br>\
+100 넘으면 경계, 126 넘으면 위험이래요<br><br><br>\
+\
+\
+식후 혈당은 식사 후 두 시간에 재 봐요<br>\
+여덟 시간까지도 식후로 본다네요<br>\
+140 넘으면 경계, 200 넘으면 위험해요<br>\
+당화혈색소 7% 넘으면 위험 신호랍니다<br><br><br>\
+\
+\
+주간 미션, <br>\
+체중 측정 포인트는 2점이래요<br><br>\
+\
+체중보다 체지방률에 신경 써야 해요<br>\
+남자는 25%, 여자는 30% 넘으면 비만,<br>\
+조금씩 줄이며, 건강을 지켜봐요<br><br><br>\
+\
+\
+건강 챙기며 포인트 따는 <br>\
+활기찬 노년이에요<br><br>\
+\
+꿩 먹고 알 먹고, 굿도 보고 떡도 먹어요<br>\
+뽕도 따고 임도 보고, <br>\
+도랑 치고 가재 잡고..<br><br>\
+\
+재밌고 건강한 '어르신 인생', <br>\
+우리 함께 걸어요~~<br><br>\
+\
+AI로 건강 관리, <br>\
+쉽고 즐겁게 함께 해요<br>\
+재미있게 운동하는 오늘도 행복합니다~~<br><br>\
+\
+<img src='https://i.imgur.com/QvCylUQ.png' width=100% title='📷 01 어르신 08_craiyon'><br>\
+</td></tr></table>",
+]
+
+const updat = "<table style=\"border-radius: 30px; BACKGROUND:#ffcccc url('') repeat fixed left top\" cellpadding='10' width=100%><tr valign=middle><td>\
+<P class='mid'><font color='blue'><b>업데이트 내역</b></font><br><br>\
+\
+<b>2025. 10. 13 (새로 25곡으로 교체)</b><br>\
+가사 1) 14곡 (1~14번),<br>\
+가사 2) 11곡 (15~25번) 선정.<br><br>\
+\
+<b>2025. 08. 10 (유료 버전 16곡 수록)</b><br>\
+가사 1) 11곡, 가사 2) 5곡 선정.<br>\
+무료 버전 모두 삭제함.<br><br>\
+\
+<b>2025. 07. 02 (무료 버전 6곡 수록)</b><br>\
+가사 1) 4곡, 가사 2) 2곡 선정.<br><br>\
+\
+<b>2025. 06. 24 (2가지 가사 생성)</b><br>\
+1) AI-IoT 기반 어르신 건강관리사업 (나의 초안 작성)<br>\
+2) AI-IoT 어르신 건강송 <br>\
+(내 초안을 ChatGPT가 개사)<br><br>\
+\
+<b>2025. 04. 18 (사업 참여)</b><br>\
+양양군 보건소 시행, <br>\
+'AI-loT 기반 어르신 건강관리사업'<br>\
+</td></tr></table>"
+
+const bon = ""
