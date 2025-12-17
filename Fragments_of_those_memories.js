5be7e71a20d98d2f46cc6d550a5157d6a25182d9
deokchan <63943073+deokchan@users.noreply.github.com>
2025-12-17 16:54:44 +0900

    Add files via upload

---
+++ Fragments_of_those_memories.js
@@ -0,0 +1,130 @@
+﻿const title0 = "'소년시대' 시리즈 삽입곡"
+
+odi = [
+"https://cdn1.suno.ai/713b8faf-feb1-4730-a3af-2099991e04f7.mp3|*[트롯] 소년시대, 그 추억의 조각들 (트로트)_MW03|",
+"https://cdn1.suno.ai/818d8351-09c6-4148-943a-93b50a88bfc8.mp3|*[발라드] 소년시대, 그 추억의 조각들 (어쿠스틱 발라드)_M07|",
+"https://cdn1.suno.ai/fceb2f8b-dc75-48c2-b054-df9d67d83eaf.mp3|*[트롯발라드] 소년시대, 그 추억의 조각들 (트로트 발라드)_MW02|",
+"https://cdn1.suno.ai/9fe0e069-2b3d-4508-a36f-6f4c10b91cb8.mp3|*[미분류] 소년시대, 그 추억의 조각들 (레트로 팝)_M04|",
+"https://cdn1.suno.ai/c435b417-9f6e-4a01-8e70-44695f2f7277.mp3|*[포크] 소년시대, 그 추억의 조각들 (포크 발라드)_M02|",
+"https://cdn1.suno.ai/41a4d3a5-d054-4512-9f61-98fe5cad09ae.mp3|*[트롯] 소년시대, 그 추억의 조각들 (트로트)_W03|",
+"https://cdn1.suno.ai/a2423c65-0501-4368-916b-1a7745e0ffec.mp3|*[발라드] 소년시대, 그 추억의 조각들 (어쿠스틱 발라드)_M08|",
+"https://cdn1.suno.ai/94785afc-ccc7-45e8-8e09-e92577cc8a23.mp3|*[미분류] 소년시대, 그 추억의 조각들 (신스팝)_M05|",
+"https://cdn1.suno.ai/1a37ecd5-e33e-4afb-bce6-6770b1539be9.mp3|[포크] 소년시대, 그 추억의 조각들 (포크 발라드)_M01|",
+"https://cdn1.suno.ai/8a3a8c42-a01a-485f-9ef0-49e33f8bba06.mp3|*[트롯] 소년시대, 그 추억의 조각들 (트로트)_W01|",
+"https://cdn1.suno.ai/f8226249-2eb0-4656-bc5f-8397c3c7ee9a.mp3|*[미분류] 소년시대, 그 추억의 조각들 (신스팝)_M06|",
+"https://cdn1.suno.ai/dda322e9-8db7-4706-a519-242acbd3a283.mp3|*[트롯] 소년시대, 그 추억의 조각들 (트로트)_W02|",
+"https://cdn1.suno.ai/0d65617e-d347-4a68-b03f-ecbd8ebadf18.mp3|[미분류] 소년시대, 그 추억의 조각들 (레트로 팝)_M03|",
+"https://cdn1.suno.ai/ae7880be-ff65-4dcb-8be8-fa4c13e26415.mp3|[트롯] 소년시대, 그 추억의 조각들 (트로트)_MW01|",
+]
+
+song = [
+`<table class='box' style="BACKGROUND:#ccffff url('https://i.imgur.com/AX2zX1C.jpeg') repeat fixed center center" cellpadding='10' width=100%><tr valign=middle><td class='lt'>
+<P class='mid'>
+<font color='blue'><b>소년시대, 그 추억의 조각들</b></font><br><br>
+
+강릉상고 졸업식 날, <br>
+폼 잡기 위해 입은 외피<br>
+사진 속엔 상일이와 백수, <br>
+그리고 옥자 일행<br><br>
+
+<img src="https://i.imgur.com/fCca3Ve.jpeg" width=100% title="📷 "><br><br>
+
+소년시대의 마지막 증표, <br>
+선명하게 남았네.<br>
+내 삶에서 <br>
+희미한 듯 가장 빛나던 순간이여!<br><br>
+
+<img src="https://i.imgur.com/zsHFFh6.jpeg" width=100% title="📷 "><br><br>
+
+머릿속 추억의 조각들을 <br>
+하나둘 모았습니다.<br><br>
+
+어설펐던 짝사랑 같은 첫사랑, <br>
+철없던 우정들..<br>
+아버지 편지 속 엄한 꾸중, <br>
+더 큰 헌신과 사랑!<br><br>
+
+<img src="https://i.imgur.com/80rCWCp.jpeg" width=100% title="📷 "><br><br>
+
+여기에, <br>
+내 마음속에, 영원히 살아서 숨 쉬네..<br><br>
+
+어머니의 위중함으로 <br>
+멈췄던 이 이야기들..<br>
+다시 펼쳐 봐도 <br>
+웃음소리와 풋풋한 설렘이<br><br>
+
+<img src="https://i.imgur.com/vaei4Jv.jpeg" width=100% title="📷 "><br><br>
+
+강릉과 양양, <br>
+친구들과 벌였던 장난과 해프닝<br>
+미소 짓게 할 이야기들이 <br>
+여기에 있었습니다<br><br><br>
+
+
+머릿속 추억의 조각들을 <br>
+하나둘 모았습니다.<br><br>
+
+어설펐던 짝사랑 같은 첫사랑, <br>
+철없던 우정들..<br>
+아버지 편지 속 엄한 꾸중, <br>
+더 큰 헌신과 사랑!<br><br>
+
+여기에, <br>
+내 마음속에, 영원히 살아서 숨 쉬네..<br><br>
+
+혹여 뒤질세라 <br>
+삶은 늘 앞으로 달려가지만,<br>
+가끔은 <br>
+이렇게 뒤돌아보는 것도 소중한 일!<br><br>
+
+열정이 고개 들어 <br>
+다시 펜을 들고 싶은 날에는<br>
+그땐 또 다른 이야기로, <br>
+또다시 시작할 겁니다<br><br>
+
+<img src="https://i.imgur.com/KxJNBch.jpeg" width=100% title="📷 "><br><br>
+
+아스라이 먼 기억의 원초는 <br>
+내 서랍 속 낙서장!<br>
+그 시절에 끄적였던 낙서들이 <br>
+글과 노래 되었네. <br><br>
+
+<img src="https://i.imgur.com/nv2sIDT.jpeg" width=100% title="📷 "><br><br>
+
+고맙습니다, <br>
+여기까지 함께 걸어와 주셔서..<br>
+내 소년시대, <br>
+마지막 페이지를 장식해 주셔서..<br><br><br>
+
+
+안녕을 기원하는 인사를 보낸다~<br><br>
+
+<img src="https://i.imgur.com/qe3DPtN.jpeg" width=100% title="📷 "><br><br>
+
+교동의 강릉상고 용봉대야<br>
+홍제동의 토끼장 친구들아<br>
+옥거리 농고 기고 친구들아<br>
+그때 그 시절의 소녀들아~<br><br>
+
+잠시만 안녕, 나의 소년시대야~~<br>
+</td></tr></table>`,
+]
+
+const updat = `<table style="border-radius: 30px; BACKGROUND:#ffcccc url('') repeat fixed left top\" cellpadding='10' width=100%><tr valign=middle><td>
+<P class='mid'><font color='red'><b>업데이트 내역</b></font><br><br>
+
+
+<font color='blue'><b>2025. 12. 17 (삽화 삽입)</b></font><br>
+가사창 삽입용 삽화 이미지 생성<br><br><br>
+
+
+<font color='blue'><b>2025. 09. 24 (해당곡 생성 완료)</b></font><br>
+"소년시대, 그 추억의 조각들" 14곡.<br><br><br>
+
+
+<font color='blue'><b>2025. 09. 04 (해당곡 생성 시작)</b></font><br>
+"소년시대, 그 추억의 조각들" 생성.
+</td></tr></table>`
+
+const bon = ""
